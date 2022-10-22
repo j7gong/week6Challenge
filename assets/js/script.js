c0c0e55ea195e3117a4039fc64827fbc5ad786fb
Jingsi <gongjingsi@hotmail.com>
2022-10-21 22:13:46 -0400

    add autocomplete when user entering a city name for search

---
+++ assets/js/script.js
@@ -0,0 +1,50 @@
+var split = function (val) {
+  return val.split(/,\s*/);
+}
+
+var extractLast = function (term) {
+  return split(term).pop();
+}
+
+var extractFirst = function (term) {
+  return split(term)[0];
+}
+
+jQuery(function () {
+  var $citiesField = jQuery("#cityInput");
+
+  $citiesField.autocomplete({
+      source: function (request, response) {
+          jQuery.getJSON(
+              "http://gd.geobytes.com/AutoCompleteCity?callback=?&q=" + extractLast(request.term),
+              function (data) {
+                  response(data);
+              }
+          );
+      },
+      minLength: 3,
+      select: function (event, ui) {
+          var selectedObj = ui.item;
+          placeName = selectedObj.value;
+          if (typeof placeName == "undefined") placeName = $citiesField.val();
+
+          if (placeName) {
+              var terms = split($citiesField.val());
+              // remove the current input
+              terms.pop();
+              // add the selected item (city only)
+              terms.push(extractFirst(placeName));
+
+              $citiesField.val(terms);
+          }
+
+          return false;
+      },
+      focus: function() {
+          // prevent value inserted on focus
+          return false;
+      },
+  });
+
+  $citiesField.autocomplete("option", "delay", 100);
+});
