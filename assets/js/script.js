24ed22c26d35e54398b4a45cf5bee6bb42d31659
Jingsi <gongjingsi@hotmail.com>
2022-10-26 19:35:00 -0400

    get future data and display it

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 var histContainer = document.querySelector("#hist");
+var futureContainer = document.querySelector("#future");
 
 // Autocomplete with cities when user searching by typing city name
 var split = function (val) {
@@ -118,6 +119,60 @@ var getCurrentWeather = function(cityName){
   });
 }
 
+//Display future weather
+var displayfuture = function (date, temp, wind, humidity) {
+  var weatherContainer = document.createElement("div");
+  weatherContainer.classList = "pr-2 color-darkBlue";
+  futureContainer.appendChild(weatherContainer);
+
+  var dateEl = document.createElement("h5");
+  dateEl.classList = "p-2 font-weight-bold";
+  dateEl.innerText = date;
+  var tempEl = document.createElement("p");
+  tempEl.className = "px-2";
+  tempEl.innerText = "Temp: "+temp+" °F";
+  var windEl = document.createElement("p");
+  windEl.className = "px-2";
+  windEl.innerText = "Wind: "+wind+" MPH";
+  var humidityEl = document.createElement("p");
+  humidityEl.className = "px-2";
+  humidityEl.innerText = "Humidity: "+humidity+" %";
+
+  weatherContainer.appendChild(dateEl);
+  weatherContainer.appendChild(tempEl);
+  weatherContainer.appendChild(windEl);
+  weatherContainer.appendChild(humidityEl);
+};
+
+//Get future weather
+var getFutureWeather = function (cityName) {
+  var city = cityName.replace(/ /g,"\%20");
+  var apiKey = "380a621c3ce6ec20318abf9243287d4c";
+  var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q="+city+",US&lang=en&units=imperial&appid="+apiKey;
+
+  fetch(apiUrl).then(function(response) {
+    if (response.ok) {
+      response.json().then(function(data) {
+        console.log(data);
+        for(i=0;i<data.list.length;i=i+8){
+          console.log(i);
+          console.log(data.list[i]);
+          console.log(data.list[i].dt_txt.split(" ")[0]);
+          console.log(data.list[i].main.temp);
+          console.log(data.list[i].wind.speed);
+          console.log(data.list[i].main.humidity);
+
+          displayfuture(data.list[i].dt_txt.split(" ")[0], data.list[i].main.temp, data.list[i].wind.speed, data.list[i].main.humidity)
+        };
+
+      });
+    } else {
+      alert('Error: GitHub User Not Found');
+    }
+  });
+
+};
+
 if (searchedCityName) {
   for(i=0; i<searchedCityName.length; i++) {
   console.log(searchedCityName[i]);
@@ -125,7 +180,5 @@ if (searchedCityName) {
   
 };
   getCurrentWeather(searchedCityName[searchedCityName.length-1]);
+  getFutureWeather(searchedCityName[searchedCityName.length-1]);
 };
-
-
-
